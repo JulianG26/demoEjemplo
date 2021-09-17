2b3d7f59e515e717ca1aed2feb6d3ab84a1067da
julyg <julyg@192.168.56.1>
2021-09-17 15:33:44 -0500

    Mi primer commit

---
+++ src/main/java/com/example/demo/DemoEjemploApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEjemploApplication.class, args);
+	}
+
+}
